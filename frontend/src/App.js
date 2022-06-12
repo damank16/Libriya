b9918882bc7aa294d120c03a06564982bd1ccaff
Sai Chand <saichand.kolloju@gmail.com>
2022-06-12 18:43:16 -0300

    CI/CD setup and added test frontend code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,14 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import AddBook from './pages/admin/AddBook'
+import Landing from './pages/Landing'
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>{/* <Route path='/admin/add' element={<AddBook />} /> */}</Routes>
+      <Routes>
+        <Route path='/admin/add' element={<AddBook />} />
+        <Route path='/' element={<Landing />} />
+      </Routes>
     </BrowserRouter>
   )
 }
