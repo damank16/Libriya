30fec9d4596e117a24203b9d061c7859d1b0b0ca
Sai Chand <saichand.kolloju@gmail.com>
2022-06-14 13:53:34 -0300

    Added dashboard ui

--- frontend/src/components/Card/MediaCard.jsx
+++ frontend/src/components/Card/MediaCard.jsx
@@ -1,18 +1,42 @@
-import { Card, CardActionArea, CardContent, CardMedia, Typography } from "@mui/material"
+import { Button, Card, CardActionArea, CardActions, CardContent, CardMedia, Grid, IconButton, Typography } from "@mui/material"
+import { Link, useNavigate } from "react-router-dom"
+import {Favorite, ShoppingCart} from '@mui/icons-material'
+import { textAlign } from "@mui/system"
 
-function MediaCard({ title, author, thumbnail}) {
+function MediaCard({ id, title, author, thumbnail, genre}) {
+
   return (
     <Card>
-
-      <CardActionArea>
+      <CardActionArea component={Link} to={`/admin/book/${id}`}>
         <CardMedia component='img' image={thumbnail} sx={{height: {
           xs: 150,
-          md: 300
+          md: 200
         }, width: {md: '100%'}}}/>
         <CardContent>
-          <Typography variant='h5'>{title}</Typography>
+          <Typography fontSize={14} fontWeight="550">{title}</Typography>
+          <Typography sx={{color: '#888'}} fontSize={12}>{genre}</Typography>
         </CardContent>
       </CardActionArea>
+      <CardActions>
+       
+        <Grid container>
+          <Grid item xs={9} sx={{display: {xs: "none", sm: "block"}}}>
+            <Button fullWidth variant="contained" color='secondary'>Add To Cart</Button>
+          </Grid>
+
+          <Grid textAlign='center' item xs={3} sx={{display: {sm: "none"}}}>
+            <IconButton>
+              <ShoppingCart size="small" />
+            </IconButton>
+          </Grid>
+
+          <Grid textAlign='center' item xs={3}>
+            <IconButton>
+              <Favorite size="small" />
+            </IconButton>
+          </Grid>
+        </Grid>
+      </CardActions>
     </Card>
   )
 }
