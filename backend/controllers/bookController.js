25b937875c033a6692241c9819754cb48d08157b
Sai Chand <saichand.kolloju@gmail.com>
2022-07-12 19:42:17 -0300

    Added Book routes and controller templates

---
+++ backend/controllers/bookController.js
@@ -0,0 +1,14 @@
+const getAllBooks = async (req, res) => {}
+
+const getBook = async (req, res) => {}
+
+const updateBook = async (req, res) => {}
+
+const deleteBook = async (req, res) => {}
+
+module.exports = {
+  getAllBooks,
+  getBook,
+  updateBook,
+  deleteBook,
+}
--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,25 @@
-const express = require("express");
-const path = require("path");
+const express = require('express')
+const path = require('path')
+const connectDatabase = require('./config/db')
+const defaultErrorHandler = require('./middlewares/errorHandler')
 
-const app = express();
+connectDatabase()
 
-app.use(express.json());
-// app.use('/', require('./routes/<folder_path>/file_name_without_extension'))
-app.use("/", require("./routes"));
+const app = express()
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/build")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "..frontend/build/index.html"));
-  });
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.use(defaultErrorHandler)
+
+app.use('/api/books', require('./routes/bookRoutes'))
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')))
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '..frontend/build/index.html'))
+  })
 }
 
-const PORT = process.env.PORT || 4000;
-app.listen(PORT, console.log(`Server running on port ${PORT}`));
+const PORT = process.env.PORT || 4000
+app.listen(PORT, console.log(`Server running on port ${PORT}`))
