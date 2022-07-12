6efab3d6f23388253c235c0630e11a98ea23d78d
Sai Chand <saichand.kolloju@gmail.com>
2022-07-12 19:42:33 -0300

    Added default error handler

---
+++ backend/middlewares/errorHandler.js
@@ -0,0 +1,12 @@
+const defaultErrorHandler = (err, req, res, next) => {
+  const statusCode = res.statusCode && 500
+
+  res.status(statusCode)
+
+  res.json({
+    message: err.message,
+    stack: process.env.NODE_ENV === 'production' ? undefined : err.stack,
+  })
+}
+
+module.exports = defaultErrorHandler
