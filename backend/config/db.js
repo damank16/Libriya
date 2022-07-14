10584e0835bc49176e6f11bd41628ac0bec8c39b
unknown <dm341240@dal.ca>
2022-07-14 01:29:09 -0300

    undo changes related to other features

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,7 @@
 const { connect } = require('mongoose')
 async function connectDatabase() {
   try {
-    const MONGO_URI = "mongodb+srv://group12:group12pwd@libriya.slwkd4n.mongodb.net/libriya?retryWrites=true&w=majority";
-    const { connection } = await connect(MONGO_URI)
+    const { connection } = await connect(process.env.MONGO_URI)
     console.log(`MongoDB connection established to ${connection.host}`)
   } catch (err) {
     console.log(err.message)
