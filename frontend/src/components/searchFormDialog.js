937bcfcc6cd2848214c3290af8cb46471afeec5a
unknown <dm341240@dal.ca>
2022-07-14 03:41:18 -0300

    handling more changes

--- frontend/src/components/searchFormDialog.js
+++ frontend/src/components/searchFormDialog.js
@@ -1,247 +1,278 @@
-import React from 'react';
-import Dialog from '@mui/material/Dialog';
-import DialogTitle from '@mui/material/DialogTitle';
-import TextField from '@mui/material/TextField';
-import DialogActions from '@mui/material/DialogActions';
-import Button from '@mui/material/Button';
-import Radio from '@mui/material/Radio';
-import RadioGroup from '@mui/material/RadioGroup';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import { Grid } from '@mui/material';
-import Typography from '@mui/material/Typography';
-import axios from 'axios';
+import React from 'react'
+import Dialog from '@mui/material/Dialog'
+import DialogTitle from '@mui/material/DialogTitle'
+import TextField from '@mui/material/TextField'
+import DialogActions from '@mui/material/DialogActions'
+import Button from '@mui/material/Button'
+import Radio from '@mui/material/Radio'
+import RadioGroup from '@mui/material/RadioGroup'
+import FormControlLabel from '@mui/material/FormControlLabel'
+import { Grid } from '@mui/material'
+import Typography from '@mui/material/Typography'
+import axios from 'axios'
 
 export default function SearchDialogForm(props) {
+  const {
+    open,
+    setDialogOpenState /*,handleClickQuery*/,
+    setChecked,
+    checked,
+    setSearchedBooks,
+    searchedBooks,
+    searchFields,
+    setSearchFields,
+    sortParameters,
+    setSortingParameters,
+  } = props
+  const [disableSearch, setDisableSearch] = React.useState(true)
+  const handleDisableSearch = () => setDisableSearch(true)
+  // const [searchFields, setSearchFields] = React.useState({
+  //     title: '',
+  //     author: '',
+  //     genre: '',
+  //     publisher: '',
+  //     publicationYear: '',
+  //   });
 
-    const {open,setDialogOpenState /*,handleClickQuery*/ ,setChecked, checked, setSearchedBooks, searchedBooks}=props;
-    const [disableSearch, setDisableSearch] = React.useState(true);
-    const handleDisableSearch = () => setDisableSearch(true);
-    const [searchFields, setSearchFields] = React.useState({
-        title: '',
-        author: '',
-        genre: '',
-        publisher: '',
-        publicationYear: '',
-      });  
-      
-      const [sortParameters, setSortingParameters] = React.useState({
-        bookName: false,
-        author: false,
-        publicationYear: false,
-      });
-    
+  //   const [sortParameters, setSortingParameters] = React.useState({
+  //     bookName: false,
+  //     author: false,
+  //     publicationYear: false,
+  //   });
 
+  const handleSearchFeildOnChanges = (event, param) => {
+    let obj = {}
+    if (param === 'publicationYear')
+      event.target.value = event.target.value.replace(/\D/g, '')
+    obj[param] = event.target.value
+    console.log(event.target.value)
+    setSearchFields((prevSearchFields) => ({
+      ...prevSearchFields,
+      ...obj,
+    }))
+  }
 
-  
-    const handleSearchFeildOnChanges = (event, param) => {
-        let obj = {};
-        if (param === 'publicationYear')
-          event.target.value = event.target.value.replace(/\D/g, '');
-        obj[param] = event.target.value;
-        console.log(event.target.value);
-        setSearchFields((prevSearchFields) => ({
-          ...prevSearchFields,
-          ...obj,
-        }));
-      };  
+  const handleSortFeildOnChanges = (event, param) => {
+    let obj = {}
+    obj[param] = event.target.value
+    console.log(event.currentTarget.value)
+    setSortingParameters((prevSearchFields) => ({
+      ...prevSearchFields,
+      ...obj,
+    }))
+  }
 
-      const handleSortFeildOnChanges = (event, param) => {
-        let obj = {};
-        obj[param] = event.target.value;
-        console.log(event.currentTarget.value);
-        setSortingParameters((prevSearchFields) => ({
-          ...prevSearchFields,
-          ...obj,
-        }));
-      };
+  const handleSearch = (event) => {
+    if (event) {
+      event.preventDefault()
+    }
+    getSearchResults()
+    console.warn('search action')
+    setDialogOpenState(false)
+    handleDisableSearch()
+  }
 
-      const handleSearch = (event) => {
-        if (event) {
-          event.preventDefault();
-        }
-        getSearchResults();
-        console.warn('search action');
-        setDialogOpenState(false);
-        handleDisableSearch();
-      };
+  const handleReset = (event) => {
+    if (event) {
+      event.preventDefault()
+    }
+    setChecked(false)
+    setSearchFields({
+      title: '',
+      author: '',
+      genre: '',
+      publisher: '',
+      publicationYear: '',
+    })
+    setDialogOpenState(false)
+  }
 
-      const getSearchResults = async () => {
-        console.log("inside getSearchResults method");
-        console.log(searchFields);
-        // const filterParams = {
-        //   "title": searchFields.title,
-        //   "author": searchFields.author,
-        //   "genre": searchFields.genre,
-        //   "publisher": searchFields.publisher,
-        //   "publication": searchFields.publicationYear
-        // };
+  const getSearchResults = async () => {
+    console.log('inside getSearchResults method')
+    console.log(searchFields)
+    // const filterParams = {
+    //   "title": searchFields.title,
+    //   "author": searchFields.author,
+    //   "genre": searchFields.genre,
+    //   "publisher": searchFields.publisher,
+    //   "publication": searchFields.publicationYear
+    // };
 
-        axios.post("http://localhost:4000/searchbooks" , {...searchFields}).then((res) => {
-          console.log(res);
-          setSearchedBooks(res.data.books);
-          setChecked(true);
-          console.log("searched books" + searchedBooks );
-        });        
-    };
+    axios
+      .post('http://localhost:4000/searchbooks', { ...searchFields })
+      .then((res) => {
+        console.log(res)
+        setSearchedBooks(res.data.books)
+        setChecked(true)
+        console.log('searched books' + searchedBooks)
+      })
+  }
 
-
-    
-
-      React.useEffect(() => {
-        const disableSearch = Object.values(searchFields).some((searchField) => {
-          console.log('searchField: ', searchField);
-          if (searchField) {
-            return true;
-          }
-          return false;
-        });
-        console.log('disableSearch: ', disableSearch);
-        setDisableSearch(!disableSearch);
-      }, [searchFields]);
-
-      const handleDialogClose = () => {
-        setDialogOpenState(false);
+  React.useEffect(() => {
+    const disableSearch = Object.values(searchFields).some((searchField) => {
+      console.log('searchField: ', searchField)
+      if (searchField) {
+        return true
       }
+      return false
+    })
+    console.log('disableSearch: ', disableSearch)
+    setDisableSearch(!disableSearch)
+  }, [searchFields])
 
+  const handleDialogClose = () => {
+    setDialogOpenState(false)
+  }
 
-
-    return(
-        <Dialog  open={open}  onBackdropClick={handleDialogClose}>
-        <DialogTitle id="search_dialog_title_id">
-          {'Search Books'}
-        </DialogTitle>
-        <Grid container spacing={2}>
-          <Grid item xs={12} md={6}>
-            <TextField
-              id="name_search_id"
-              label="Book Name"
-              variant="outlined"
-              onChange={(event) => {
-                handleSearchFeildOnChanges(event, 'title');
-              }}
-              sx={{
-                marginLeft: '10px',
-                marginRight: '10px',
-                width: 'calc(100% - 20px)',
-              }}
-            />
-          </Grid>
-          <Grid item xs={12} md={6}>
-            {/* Code Attribution 1*/}
-            {/* [URL]:hhttps://mui.com/material-ui/react-text-field/ */}
-            <TextField
-              id="author_search_id"
-              label="Author"
-              variant="outlined"
-              onChange={(event) => {
-                handleSearchFeildOnChanges(event, 'author');
-              }}
-              sx={{
-                marginLeft: '10px',
-                marginRight: '10px',
-                width: 'calc(100% - 20px)',
-              }}
-            />
-          </Grid>
-          <Grid item xs={12} md={6}>
-            <TextField
-              id="subject_search_id"
-              label="Genre"
-              variant="outlined"
-              onChange={(event) => {
-                handleSearchFeildOnChanges(event, 'genre');
-              }}
-              sx={{
-                marginLeft: '10px',
-                marginRight: '10px',
-                width: 'calc(100% - 20px)',
-              }}
-            />
-          </Grid>
-          <Grid item xs={12} md={6}>
-            <TextField
-              id="publication_search_id"
-              label="Publication"
-              variant="outlined"
-              onChange={(event) => {
-                handleSearchFeildOnChanges(event, 'publisher');
-              }}
-              sx={{
-                marginLeft: '10px',
-                marginRight: '10px',
-                width: 'calc(100% - 20px)',
-              }}
-            />
-          </Grid>
-          <Grid item xs={12} md={6}>
-            <TextField
-              id="publication_year_search_id"
-              label="Publication Year (YYYY)"
-              variant="outlined"
-              //type="number"
-              inputProps={{ maxLength: 4 }}
-              // inputProps={{  }}
-              onChange={(event) => {
-                handleSearchFeildOnChanges(event, 'publicationYear');
-              }}
-              sx={{
-                marginLeft: '10px',
-                marginRight: '10px',
-                width: 'calc(100% - 20px)',
-              }}
-            />
-          </Grid>
+  return (
+    <Dialog open={open} onBackdropClick={handleDialogClose}>
+      <DialogTitle id='search_dialog_title_id'>{'Search Books'}</DialogTitle>
+      <Grid container spacing={2}>
+        <Grid item xs={12} md={6}>
+          <TextField
+            id='name_search_id'
+            label='Book Name'
+            value={searchFields.title}
+            variant='outlined'
+            onChange={(event) => {
+              handleSearchFeildOnChanges(event, 'title')
+            }}
+            sx={{
+              marginLeft: '10px',
+              marginRight: '10px',
+              width: 'calc(100% - 20px)',
+            }}
+          />
         </Grid>
-        <br />
-        <Typography
-          sx={{
-            marginLeft: '10px',
-            marginRight: '10px',
-            width: 'calc(100% - 20px)',
+        <Grid item xs={12} md={6}>
+          {/* Code Attribution 1*/}
+          {/* [URL]:hhttps://mui.com/material-ui/react-text-field/ */}
+          <TextField
+            id='author_search_id'
+            label='Author'
+            value={searchFields.author}
+            variant='outlined'
+            onChange={(event) => {
+              handleSearchFeildOnChanges(event, 'author')
+            }}
+            sx={{
+              marginLeft: '10px',
+              marginRight: '10px',
+              width: 'calc(100% - 20px)',
+            }}
+          />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <TextField
+            id='subject_search_id'
+            label='Genre'
+            variant='outlined'
+            onChange={(event) => {
+              handleSearchFeildOnChanges(event, 'genre')
+            }}
+            sx={{
+              marginLeft: '10px',
+              marginRight: '10px',
+              width: 'calc(100% - 20px)',
+            }}
+          />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <TextField
+            id='publication_search_id'
+            label='Publication'
+            variant='outlined'
+            onChange={(event) => {
+              handleSearchFeildOnChanges(event, 'publisher')
+            }}
+            sx={{
+              marginLeft: '10px',
+              marginRight: '10px',
+              width: 'calc(100% - 20px)',
+            }}
+          />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <TextField
+            id='publication_year_search_id'
+            label='Publication Year (YYYY)'
+            variant='outlined'
+            //type="number"
+            inputProps={{ maxLength: 4 }}
+            // inputProps={{  }}
+            onChange={(event) => {
+              handleSearchFeildOnChanges(event, 'publicationYear')
+            }}
+            sx={{
+              marginLeft: '10px',
+              marginRight: '10px',
+              width: 'calc(100% - 20px)',
+            }}
+          />
+        </Grid>
+      </Grid>
+      <br />
+      <Typography
+        sx={{
+          marginLeft: '10px',
+          marginRight: '10px',
+          width: 'calc(100% - 20px)',
+        }}
+      >
+        Sort By
+      </Typography>
+      <RadioGroup
+        row
+        aria-labelledby='demo-row-radio-buttons-group-label'
+        name='row-radio-buttons-group'
+        sx={{
+          marginLeft: '10px',
+          marginRight: '10px',
+          width: 'calc(100% - 20px)',
+        }}
+      >
+        <FormControlLabel
+          value='name'
+          control={<Radio />}
+          label='Name'
+          onChange={(event) => {
+            handleSortFeildOnChanges(event, 'title')
+          }}
+        />
+        <FormControlLabel
+          value='author'
+          control={<Radio />}
+          label='Author'
+          onChange={(event) => {
+            handleSortFeildOnChanges(event, 'author')
+          }}
+        />
+        <FormControlLabel
+          value='publication_year'
+          control={<Radio />}
+          label='Publication Year'
+          onChange={(event) => {
+            handleSortFeildOnChanges(event, 'publicationYear')
+          }}
+        />
+      </RadioGroup>
+      <DialogActions>
+        <Button
+          disabled={disableSearch}
+          onClick={(e) => {
+            handleSearch(e)
           }}
         >
-          Sort By
-        </Typography>
-        <RadioGroup
-          row
-          aria-labelledby="demo-row-radio-buttons-group-label"
-          name="row-radio-buttons-group"
-          sx={{
-            marginLeft: '10px',
-            marginRight: '10px',
-            width: 'calc(100% - 20px)',
+          Search
+        </Button>
+        <Button
+          onClick={(e) => {
+            handleReset(e)
           }}
         >
-          <FormControlLabel
-            value="name"
-            control={<Radio />}
-            label="Name"
-            onChange={(event) => {
-              handleSortFeildOnChanges(event, 'title');
-            }}
-          />
-          <FormControlLabel
-            value="author"
-            control={<Radio />}
-            label="Author"
-            onChange={(event) => {
-              handleSortFeildOnChanges(event, 'author');
-            }}
-          />
-          <FormControlLabel
-            value="publication_year"
-            control={<Radio />}
-            label="Publication Year"
-            onChange={(event) => {
-              handleSortFeildOnChanges(event, 'publicationYear');
-            }}
-          />
-        </RadioGroup>
-        <DialogActions>
-          <Button disabled={disableSearch} onClick={(e) => { handleSearch(e) }}>
-            Search
-          </Button>
-        </DialogActions>
-      </Dialog>
-    );
-};
+          Reset
+        </Button>
+      </DialogActions>
+    </Dialog>
+  )
+}
--- frontend/src/pages/dashboard/Dashboard.js
+++ frontend/src/pages/dashboard/Dashboard.js
@@ -2,24 +2,38 @@ import { useState, useEffect } from 'react'
 import { Grid, Typography } from '@mui/material'
 import MediaCard from '../../components/Card/MediaCard'
 //import books from '../../data/books'
-import Filter from '../../components/filterMenu';
-import SearchIcon from '@mui/icons-material/Search';
-import SearchDialogForm from '../../components/searchFormDialog';
-import axios from 'axios';
-import Button from '@mui/material/Button';
+import Filter from '../../components/filterMenu'
+import SearchIcon from '@mui/icons-material/Search'
+import SearchDialogForm from '../../components/searchFormDialog'
+import axios from 'axios'
+import Button from '@mui/material/Button'
 import Spinner from '../../components/common/Spinner'
 
 function Dashboard() {
-  const [checked, setChecked] = useState(false);
-  const [searchDialogOpen, setsearchDialogOpen] = useState(false);
-  const [searchedBooks, setSearchedBooks] = useState([]);
-  const handleSearchDialogOpen = () => setsearchDialogOpen(true);
+  const [checked, setChecked] = useState(false)
+  const [searchDialogOpen, setsearchDialogOpen] = useState(false)
+  const [searchedBooks, setSearchedBooks] = useState([])
+  const handleSearchDialogOpen = () => setsearchDialogOpen(true)
   const [books, setBooks] = useState([])
   const [loading, setLoading] = useState(false)
 
-useEffect(() => {
-    console.log("searchedBooks test: ", searchedBooks);
-  }, [searchedBooks]);
+  const [searchFields, setSearchFields] = useState({
+    title: '',
+    author: '',
+    genre: '',
+    publisher: '',
+    publicationYear: '',
+  })
+
+  const [sortParameters, setSortingParameters] = useState({
+    bookName: false,
+    author: false,
+    publicationYear: false,
+  })
+
+  useEffect(() => {
+    console.log('searchedBooks test: ', searchedBooks)
+  }, [searchedBooks])
 
   useEffect(() => {
     ;(async () => {
@@ -34,53 +48,89 @@ useEffect(() => {
     })()
   }, [])
 
+  const renderBooks = () => {
+    if (loading) return <Spinner />
+    return !checked ? (
+      books.map((book) => (
+        <Grid item md={3} sm={4} xs={6}>
+          <MediaCard key={book.id} {...book} />
+        </Grid>
+      ))
+    ) : searchedBooks.length ? (
+      searchedBooks.map((book) => (
+        <Grid item md={3} sm={4} xs={6}>
+          <MediaCard key={book.id} {...book} />
+        </Grid>
+      ))
+    ) : (
+      <Grid item md={3} sm={4} xs={6}>
+        {' '}
+        No Books Found{' '}
+      </Grid>
+    )
+  }
 
   return (
-    
     <>
-    <Grid container sx={{
-    flexFlow: 'row',
-    width: '100%'}}>
-      <Filter  setChecked={setChecked} checked= {checked}  setSearchedBooks = {setSearchedBooks} searchedBooks = {searchedBooks}/>
-        <Grid container sx={{
-          display: 'flex',
-          flexFlow: 'column nowrap',
-          marginLeft :'2%'}}>
-            <Grid  container sx = {{
-                flexFlow: 'row',
-                justifyContent : 'space-between',}}>
-              <Typography variant='h4' my={1}>
-                Books
-              </Typography>
-              <SearchIcon sx={{display:{ sx:'flex', md: 'none'} }} onClick={handleSearchDialogOpen} />
-              {/* <Button variant="contained" onClick={(e) => { resetFilters(e) }}   sx = {{
+      <Grid
+        container
+        sx={{
+          flexFlow: 'row',
+          width: '100%',
+        }}
+      >
+        <Filter
+          setChecked={setChecked}
+          checked={checked}
+          setSearchedBooks={setSearchedBooks}
+          searchedBooks={searchedBooks}
+        />
+        <Grid
+          container
+          sx={{
+            display: 'flex',
+            flexFlow: 'column nowrap',
+            marginLeft: '2%',
+          }}
+        >
+          <Grid
+            container
+            sx={{
+              flexFlow: 'row',
+              justifyContent: 'space-between',
+            }}
+          >
+            <Typography variant='h4' my={1}>
+              Books
+            </Typography>
+            <SearchIcon
+              sx={{ display: { sx: 'flex', md: 'none' } }}
+              onClick={handleSearchDialogOpen}
+            />
+            {/* <Button variant="contained" onClick={(e) => { resetFilters(e) }}   sx = {{
               margin:'2px',
               display:{ sx:'flex', md: 'none'} 
           }}>Reset</Button> */}
-              <SearchDialogForm open={searchDialogOpen}  
+            <SearchDialogForm
+              open={searchDialogOpen}
               // onClose={setsearchDialogOpen}
               setDialogOpenState={setsearchDialogOpen}
-              setChecked={setChecked} 
-              checked= {checked}  
-              setSearchedBooks = {setSearchedBooks} 
-              searchedBooks = {searchedBooks}
+              setChecked={setChecked}
+              checked={checked}
+              setSearchedBooks={setSearchedBooks}
+              searchedBooks={searchedBooks}
+              searchFields={searchFields}
+              setSearchFields={setSearchFields}
+              sortParameters={sortParameters}
+              setSortingParameters={setSortingParameters}
               //handleClickQuery  = {handleClickQuery}
-              /> 
-            </Grid>
-            <Grid container spacing={3} rowGap={2}>
-              {!checked ? books.map((book) => (
-                <Grid item md={3} sm={4} xs={6}>
-                  <MediaCard key={book.id} {...book} />
-                </Grid>
-              )):
-                searchedBooks.length ? searchedBooks.map((book) => (
-                <Grid item md={3} sm={4} xs={6}>
-                  <MediaCard key={book.id} {...book} />
-                </Grid>)) : <Grid item md={3} sm={4} xs={6}> No Books Found </Grid>
-                }
-            </Grid>
+            />
+          </Grid>
+          <Grid container spacing={3} rowGap={2}>
+            {renderBooks()}
+          </Grid>
         </Grid>
-    </Grid>
+      </Grid>
     </>
   )
 }
