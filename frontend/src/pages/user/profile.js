ee4f64a4c2919295d1f6b01fbfb493071321bdb2
unknown <dm341240@dal.ca>
2022-07-15 19:41:49 -0300

    Fixing pipeline errors

--- frontend/src/pages/user/profile.js
+++ frontend/src/pages/user/profile.js
@@ -9,14 +9,14 @@ import {
   Button,
   Grid,
 } from '@mui/material'
-// import moment from "moment";
+import moment from 'moment'
 import { useNavigate } from 'react-router-dom'
 import Favorites from './favorites'
 import { useEffect, useState } from 'react'
 import axios from 'axios'
 
 const Profile = () => {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     axios
@@ -26,47 +26,47 @@ const Profile = () => {
         },
       })
       .then((res) => {
-        console.log({ res });
-        setUser(res.data.user);
+        console.log({ res })
+        setUser(res.data.user)
       })
       .catch((error) => {
-        console.log(error);
-      });
-  }, []);
-  const navigate = useNavigate();
-  console.log(user);
+        console.log(error)
+      })
+  }, [])
+  const navigate = useNavigate()
+  console.log(user)
   return (
     user && (
-      <Container maxWidth="lg">
+      <Container maxWidth='lg'>
         <Paper sx={{ p: 1.5, my: 1 }}>
           <Stack
-            direction="row"
-            justifyContent="space-between"
-            alignItems="center"
+            direction='row'
+            justifyContent='space-between'
+            alignItems='center'
             spacing={2}
           >
-            <Box sx={{ display: "flex", alignItems: "center" }}>
+            <Box sx={{ display: 'flex', alignItems: 'center' }}>
               <Avatar
                 sx={{
-                  bgcolor: "#2c3e50",
-                  height: "100px",
-                  width: "100px",
+                  bgcolor: '#2c3e50',
+                  height: '100px',
+                  width: '100px',
                   mr: 4,
                   ml: 8,
                 }}
-                alt="Remy Sharp"
+                alt='Remy Sharp'
               />
-              <Stack sx={{ display: "flex", flex: 1, m: 1 }}>
-                <Typography variant="h4">
+              <Stack sx={{ display: 'flex', flex: 1, m: 1 }}>
+                <Typography variant='h4'>
                   {user.firstName} {user.lastName}
                 </Typography>
-                <Typography variant="subtitle">{user.email}</Typography>
+                <Typography variant='subtitle'>{user.email}</Typography>
               </Stack>
             </Box>
-            <Grid justifyContent="flex-end">
+            <Grid justifyContent='flex-end'>
               <Button
-                variant="contained"
-                onClick={() => navigate("/edit-profile", { state: { user } })}
+                variant='contained'
+                onClick={() => navigate('/edit-profile', { state: { user } })}
               >
                 Edit Profile
               </Button>
@@ -145,22 +145,22 @@ const Profile = () => {
           </Stack> */}
 
           <Typography
-            variant="overline"
-            justifyContent="flex-end"
-            display="flex"
+            variant='overline'
+            justifyContent='flex-end'
+            display='flex'
             mt={2}
           >
-            Member Since: {moment(user.createdAt).format("MMMM Do YYYY")}
+            Member Since: {moment(user.createdAt).format('MMMM Do YYYY')}
           </Typography>
         </Paper>
 
-        <Typography variant="h6" justifyContent="center" display="flex" my={4}>
+        <Typography variant='h6' justifyContent='center' display='flex' my={4}>
           My Favorites
         </Typography>
         {user && <Favorites user={user} />}
       </Container>
     )
-  );
-};
+  )
+}
 
 export default Profile
