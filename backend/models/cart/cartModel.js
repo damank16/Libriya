c4555d26ba25d92bcbd28fcea45f0f6c9e89a350
vanshika <vn232426@dal.ca>
2022-07-17 13:58:40 -0300

    Refactored code and modified for fine calculation

--- backend/models/cart/cartModel.js
+++ backend/models/cart/cartModel.js
@@ -1,12 +1,14 @@
 /* Authored by Vanshika Gohel, B00888111, vn232426@dal.ca
  */
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
 const cartSchema = mongoose.Schema({
-    user_id: String,
-    bookId: String,
-    checkout_date: String,
-    checkin_date: String
+  user_id: String,
+  bookId: String,
+  checkout_date: String,
+  checkin_date: String,
+  due_date: String,
+  isFineDue: Boolean,
 })
 
-exports.Cart = mongoose.model('cart', cartSchema);
+exports.Cart = mongoose.model('cart', cartSchema)
