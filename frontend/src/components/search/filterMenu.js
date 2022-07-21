0bbb9c64bac7f404ce7154ae3e1cdc3cd664b74b
Sai Chand <saichand.kolloju@gmail.com>
2022-07-21 13:01:00 -0300

    Added close button to search dialog

--- frontend/src/components/search/filterMenu.js
+++ frontend/src/components/search/filterMenu.js
@@ -105,7 +105,7 @@ export default function Filter({
 
   const getSearchResults = async () => {
     axios
-      .post('/searchbooks', {
+      .post('/api/searchbooks', {
         ...filterParams,
         sort: sortMethod,
       })
